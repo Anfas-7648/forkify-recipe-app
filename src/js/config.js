cbb0f008bee1b9be3f8c03cc8834a0960111b5ee
Mohammed Anfas C <anfas7648@gmail.com>
2022-12-18 09:48:16 +0530

    final

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '<9506abc0-dbe4-42cb-9736-865ebe9561cc>';
