29fa8e2dd21719be7ce1e2a290109551628dee46
Mohammed Anfas C <anfas7648@gmail.com>
2022-12-18 09:22:06 +0530

    fraction

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<8cfcc7d2-5bd1-4baa-b140-4bf6a3450588>';
+export const KEY = '<9506abc0-dbe4-42cb-9736-865ebe9561cc>';
 export const MODAL_CLOSE_SEC = 2.5;
