282c7b2437ce5d6e19a34d240c3295bd4e9e2a65
Mohammed Anfas C <anfas7648@gmail.com>
2022-12-19 11:07:11 +0530

    new key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<9506abc0-dbe4-42cb-9736-865ebe9561cc>';
+export const KEY = '<7569c0a6-480c-477a-9ebb-20772503e1aa>';
 export const MODAL_CLOSE_SEC = 2.5;
